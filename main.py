b0ed9cf5b3db7675f359eac8e7a196b6ade43f11
saanvi-kanodia <saanvikanodia05@gmail.com>
2025-08-10 20:06:30 +0530

    requirements file created

---
+++ main.py
@@ -0,0 +1,82 @@
+import os
+import getpass
+import faiss
+from langchain.chat_models import init_chat_model
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community.docstore.in_memory import InMemoryDocstore
+from langchain_community.vectorstores import FAISS
+
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+
+MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
+HF_TOKEN=os.getenv("HF_TOKEN")
+LANGSMITH_TRACING=os.getenv("LANGSMITH_API_KEY")
+USER_AGENT=os.getenv("USER_AGENT")
+
+llm = init_chat_model("mistral-large-latest", model_provider="mistralai")
+embeddings = MistralAIEmbeddings(model="mistral-embed")
+
+embedding_dim = 1024
+index = faiss.IndexFlatL2(embedding_dim)
+
+vector_store = FAISS(
+    embedding_function=embeddings,
+    index=index,
+    docstore=InMemoryDocstore(),
+    index_to_docstore_id={},
+)
+
+# Load and chunk contents of the blog
+loader = WebBaseLoader(
+    web_paths=("https://purplescape.com/the-ultimate-guide-to-understanding-advanced-retrieval-augmented-generation-methodologies/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+# Index chunks
+_ = vector_store.add_documents(documents=all_splits)
+
+prompt = hub.pull("rlm/rag-prompt")
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+# Define application steps
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+# Compile application and test
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+response = graph.invoke({"question": "What are the three main points of this document?"})
+print(response["answer"])
+
